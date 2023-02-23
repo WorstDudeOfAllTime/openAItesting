236372e5ada54027c6f2152080a6f33fcfe76270
Worstdudeofalltime <Krisbertolino@gmail.com>
2023-02-22 21:57:56 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+import { useEffect, useState, useRef } from 'react';
+const { Configuration, OpenAIApi } = require('openai');
+import {ChatGPTUnofficialProxyAPI} from 'chatgpt'
+const configuration = new Configuration({
+  apiKey: ''
+})
+const inter = Inter({ subsets: ['latin'] });
+const openai = new OpenAIApi(configuration);
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [messages, setMessages] = useState([]);
+  const messageRef = useRef();
+  const addMessage = (message) => {
+    setMessages([...messages, message]);
+  };
+  const fireMessageGPT = async (message) => {
+    const api = new ChatGPTUnofficialProxyAPI({
+      accessToken: ""
+    })
+    const res = await api.sendMessage(message)
+    console.log(res.text)
+  }
+  const fireMessage = async (message) => {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: message,
+      "max_tokens": 2048,
+      temperature: .5
+    });
+    const data = response.data.choices[0].text;
+    console.log(response.data.choices[0])
+    addMessage(data);
+  };
   return (
     <>
       <Head>
@@ -15,109 +45,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div>
+          {messages.map((message, index) => {
+            return <div key={index}>{message}</div>;
+          })}
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            fireMessage(messageRef.current.value);
+            fireMessageGPT(messageRef.current.value);
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Enter your message"
+            name="messages"
+            ref={messageRef}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+          <button type="submit">Send Message</button>
+        </form>
       </main>
     </>
-  )
+  );
 }
